db42292ee7b376018b140e6aff073424ca38deff
choimory <43328589+choimory@users.noreply.github.com>
2021-08-08 14:32:56 +0900

    Initial commit

---
+++ src/main/java/com/choimory/helloelasticsearch/HelloElasticSearchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.helloelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloElasticSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloElasticSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/helloelasticsearch/HelloElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.helloelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloElasticSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
